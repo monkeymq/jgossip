3b95b18523819c5ed23d9fe918001fde8473cca3
monkeymq <legendof01@gmail.com>
2017-08-01 09:45:24 +0800

    Update UDPMsgService.java

--- src/main/java/net/lvsq/jgossip/net/udp/UDPMsgService.java
+++ src/main/java/net/lvsq/jgossip/net/udp/UDPMsgService.java
@@ -1,3 +1,17 @@
+// Copyright (c) 2017 The jgossip Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package net.lvsq.jgossip.net.udp;
 
 import io.netty.util.internal.StringUtil;
@@ -16,6 +30,9 @@ import net.lvsq.jgossip.handler.SyncMessageHandler;
 import net.lvsq.jgossip.model.MessageType;
 import net.lvsq.jgossip.net.MsgService;
 
+/**
+ * @author lvsq
+ */
 public class UDPMsgService implements MsgService {
     DatagramSocket socket;
 
