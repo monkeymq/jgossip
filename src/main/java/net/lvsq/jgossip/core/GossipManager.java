704064e50c14ef074d8110275c6ee9ed2669293c
monkeymq <legendof01@gmail.com>
2017-08-01 09:30:18 +0800

    Update GossipManager.java

--- src/main/java/net/lvsq/jgossip/core/GossipManager.java
+++ src/main/java/net/lvsq/jgossip/core/GossipManager.java
@@ -1,3 +1,17 @@
+// Copyright (c) 2017 The jgossip Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package net.lvsq.jgossip.core;
 
 import io.vertx.core.buffer.Buffer;
@@ -28,6 +42,9 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
+/**
+ * @author lvsq
+ */
 public class GossipManager {
     private static final Logger LOGGER = LoggerFactory.getLogger(GossipManager.class);
     private static GossipManager instance = new GossipManager();
