db275c19fe2fb30ac8e4322d6e10a325a8959a7a
siquanlv <silv@cisco.com>
2025-06-23 08:38:35 +0800

    ai code review

--- src/test/java/net/lvsq/jgossip/BuggyCalculator.java
+++ src/test/java/net/lvsq/jgossip/BuggyCalculator.java
@@ -9,7 +9,6 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class BuggyCalculator {
-
     private static final Logger logger = Logger.getLogger(BuggyCalculator.class.getName());
 
     public double divide(int numerator, int denominator) {
